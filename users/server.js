209ac7a185a7144168a9a7d5a88cca1fad044026
Bunks18 <wbunker11@gmail.com>
2018-01-30 20:29:35 -0500

    first push

---
+++ users/server.js
@@ -0,0 +1,21 @@
+// bringing in express
+const express = require('express')
+
+//bring in glue layer
+const expressGraphQL = require('express-graphql');
+
+const schema = require('./schemas/schema');
+
+//using express to create a server instance
+const app = express();
+
+app.use('/graphQL', expressGraphQL({
+  schema,
+  graphiql: true
+}))
+
+
+// takes two arguements, the port and the callback function
+app.listen(4000, () =>{
+  console.log('listening betch');
+});
