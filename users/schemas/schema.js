316b3d5020fae7c2a0690df11fe21098d4265178
Bunks18 <wbunker11@gmail.com>
2018-01-31 20:31:32 -0500

    added fragments

--- users/schemas/schema.js
+++ users/schemas/schema.js
@@ -7,7 +7,8 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -19,7 +20,7 @@ const CompanyType = new GraphQLObjectType({
     users: {
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
-       return axios
+        return axios
           .get(`http://localhost:3000/company/${parentValue.id}/users`)
           .then(res => res.data);
       }
@@ -68,6 +69,37 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull (GraphQLString) },
+        age: { type: new GraphQLNonNull (GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, {firstName, age}) {
+        return axios.post(`http://localhost:3000/users/`, {firstName, age})
+        .then(res => res.data)
+      }
+    },
+    deleteUser:{
+      type: UserType,
+      args: {
+        id: {type: GraphQLString}
+      },
+      resolve(parentValue, {id}){
+        return axios.delete(`http://localhost:3000/users/${id}`)
+        .then(res => res.data)
+      }
+    }
+  }
+});
+
+//
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
